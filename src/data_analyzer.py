13b4ba84c855a7b8b1e97483279050d8c88f0a6f
Attila Zsiros <attila.zsiros@outlook.com>
2024-03-26 16:05:25 +0100

    refactoring

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -19,8 +19,7 @@ class DataAnalyzer:
 
     @staticmethod
     def img_name_to_date(img_name: str) -> str:
-        dt = datetime.strptime(Path(img_name).stem, '%Y%m%d_%H%M%S')
-        return dt  # f"{dt.day}.{dt.month}.{dt.year}"
+        return datetime.strptime(Path(img_name).stem, '%Y%m%d_%H%M%S')
 
     def discard_outliers_LOF(self, df: pd.DataFrame):
         df = df.dropna()
@@ -30,31 +29,20 @@ class DataAnalyzer:
         y_pred = clf.fit_predict(X)
         n_outliers = sum(y_pred == -1)
 
-        # df.loc[y_pred == -1, "digits"] = 0
-
         return df[y_pred == 1], n_outliers
-
-    def discard_outliers_quantile(self, q=0.8):
-        n_orig = len(self.df)
-        q = self.df["digits"].quantile(0.8)
-        df = self.df[self.df["digits"] < q]
-        n_outliers = n_orig - len(df)
-        return df, n_outliers
 
     def analyze(self):
         n_orig = len(self.df)
-        n_na = self.df.isna().sum()
+        n_na = self.df["digits"].isna().sum()
 
-        # self.df, n_outliers = self.discard_outliers_quantile(self.df)
         self.df, n_outliers = self.discard_outliers_LOF(self.df)
 
-        # self.df.update(df_filtered, overwrite=True)
-
         print(
-            f"Original size: {n_orig}, NaN count: {n_na}, outlier count: {n_outliers}"
+            f"Original size: {n_orig}, NaN count: {n_na}, Outlier count: {n_outliers}"
         )
 
         plt.plot(self.df["date"], self.df["digits"], marker='o')
+        plt.title("Gas consumption [m3]")
         plt.gcf().autofmt_xdate()
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y'))
         plt.grid()
