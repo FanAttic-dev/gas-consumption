313e1eb7cfa2b8468abd97e06fa8f1f08fc1b641
Attila Zsiros <attila.zsiros@outlook.com>
2024-03-26 15:55:31 +0100

    set outliers to zero

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -9,9 +9,9 @@ from datetime import datetime
 class DataAnalyzer:
     def __init__(self, csv_name: str):
         self.csv_name = csv_name
-        self.df = pd.read_csv(csv_name, decimal=',')
-        self.df["digits"] = self.df["digits"].astype(float)
+        self.df = pd.read_csv(csv_name, decimal=',', index_col="idx")
         self.df = self.df.sort_values("img_name")
+        self.df["digits"] = self.df["digits"].astype(float)
         self.df["date"] = self.df["img_name"].apply(
             DataAnalyzer.img_name_to_date
         )
@@ -21,14 +21,17 @@ class DataAnalyzer:
         dt = datetime.strptime(Path(img_name).stem, '%Y%m%d_%H%M%S')
         return f"{dt.day}.{dt.month}.{dt.year}"
 
-    def discard_outliers_LOF(self, df):
+    def discard_outliers_LOF(self, df: pd.DataFrame):
+        df = df.dropna()
         clf = LocalOutlierFactor(n_neighbors=20, contamination=0.3)
         X = np.reshape(df["digits"], (-1, 1))
 
         y_pred = clf.fit_predict(X)
         n_outliers = sum(y_pred == -1)
 
-        return self.df[y_pred == 1], n_outliers
+        df.loc[y_pred == -1, "digits"] = 0
+
+        return df, n_outliers
 
     def discard_outliers_quantile(self, q=0.8):
         n_orig = len(self.df)
@@ -38,14 +41,13 @@ class DataAnalyzer:
         return df, n_outliers
 
     def analyze(self):
-
         n_orig = len(self.df)
-
-        self.df = self.df.dropna()
-        n_na = n_orig - len(self.df)
+        n_na = self.df.isna().sum()
 
         # self.df, n_outliers = self.discard_outliers_quantile(self.df)
-        self.df, n_outliers = self.discard_outliers_LOF(self.df)
+        df_filtered, n_outliers = self.discard_outliers_LOF(self.df)
+
+        self.df.update(df_filtered, overwrite=True)
 
         print(
             f"Original size: {n_orig}, NaN count: {n_na}, outlier count: {n_outliers}"
