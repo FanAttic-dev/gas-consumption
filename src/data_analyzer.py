16739bb55aeca14acb363252c2bf6ab83fc1a18d
Attila Zsiros <attila.zsiros@outlook.com>
2024-03-29 11:14:14 +0100

    plot data

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -22,7 +22,10 @@ class DataAnalyzer:
     def img_name_to_date(img_name: str) -> str:
         return datetime.strptime(Path(img_name).stem, '%Y%m%d_%H%M%S')
 
-    def discard_outliers_LOF(self, df: pd.DataFrame):
+    def discard_na_outliers_LOF(self, df: pd.DataFrame) -> pd.DataFrame:
+        n_orig = len(df)
+        n_na = df["digits"].isna().sum()
+
         df = df.dropna()
         clf = LocalOutlierFactor(n_neighbors=20, contamination=0.3)
         X = np.reshape(df["digits"], (-1, 1))
@@ -30,21 +33,34 @@ class DataAnalyzer:
         y_pred = clf.fit_predict(X)
         n_outliers = sum(y_pred == -1)
 
-        return df[y_pred == 1], n_outliers
-
-    def analyze(self):
-        n_orig = len(self.df)
-        n_na = self.df["digits"].isna().sum()
-
-        self.df, n_outliers = self.discard_outliers_LOF(self.df)
-
         print(
             f"Original size: {n_orig}, NaN count: {n_na}, Outlier count: {n_outliers}"
         )
 
+        return df[y_pred == 1]
+
+    def plot_gas_meter_values(self):
+        plt.figure()
         plt.plot(self.df.index, self.df["digits"], marker='o')
-        plt.title("Gas consumption [m3]")
+        plt.title("Gas meter values [m3]")
         plt.gcf().autofmt_xdate()
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y'))
         plt.grid()
+
+    def plot_mean_gas_consumption_per_month(self):
+        consumption_per_month = self.df.groupby(pd.Grouper(freq='ME'))[
+            ['digits']].last().diff().rename(columns={'digits': 'consumption'})
+        mean_consumption_per_month = consumption_per_month.groupby(
+            consumption_per_month.index.month).mean().rename_axis("month")
+
+        mean_consumption_per_month.plot.bar()
+        plt.title('Mean gas consumption per month')
+        plt.ylabel("Gas consumption [m3]")
+        plt.xlabel("Month")
+        plt.legend().remove()
+
+    def analyze(self):
+        self.df = self.discard_na_outliers_LOF(self.df)
+        self.plot_gas_meter_values()
+        self.plot_mean_gas_consumption_per_month()
         plt.show()
