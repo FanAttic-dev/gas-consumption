e19928db00a1319ad6b1ecc205575009a2889dbb
Attila Zsiros <attila.zsiros@outlook.com>
2024-04-02 11:46:30 +0200

    add db

--- src/backend/services/models.py
+++ src/backend/services/models.py
@@ -1,4 +1,4 @@
-from services.database import db
+from app import db
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -6,26 +6,29 @@ class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     
-    def __init__(self, username, password):
-        self.username = username
+    def __init__(self, name, password):
+        self.name = name
         self.password = generate_password_hash(password, method='sha256')
         
     @classmethod
     def authenticate(cls, **kwargs):
-        username = kwargs.get('username')
+        name = kwargs.get('name')
         password = kwargs.get('password')
         
-        if not username or not password:
+        if not name or not password:
             return None
         
-        user = cls.query.filter_by(username=username).first()
+        user = cls.query.filter_by(name=name).first()
         if not user or not check_password_hash(user.password, password):
             return None
         
         return user
 
     def to_dict(self):
-        return dict(id=self.id, username=self.username)
+        return dict(id=self.id, name=self.name)
+    
+    def __repr__(self):
+        return f"<User {self.name}>"
